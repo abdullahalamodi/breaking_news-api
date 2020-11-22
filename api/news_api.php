86e90def3f052ff1d22a3ca3e144fecddac64448
abdullahalamodi <39647459+abdullahalamodi@users.noreply.github.com>
2020-11-22 16:44:14 +0300

    fix delete function

--- api/news_api.php
+++ api/news_api.php
@@ -1,33 +1,40 @@
 <?php
 include('../models/News.php');
 $news = new News();
-$method = $_SERVER['REQUEST_MERHOD'];
+$method = $_SERVER['REQUEST_METHOD'];
 //post
 if (isset($_POST) && !empty($_POST)) {
-    $news->id = $_POST['id'];
-    $news->title = $_POST['title'];
-    $news->image = $_POST['image'];
-    $news->details = $_POST['details'];
-    $news->category_id = $_POST['category_id'];
+    //update
+    if ($_GET['id'] >= 0) {
+        $news->title = $_POST['title'];
+        $news->image = $_POST['image'];
+        $news->details = $_POST['details'];
+        $news->category_id = $_POST['category_id'];
 
-    if ($news->addNews()) {
-        echo "news added successfuly ^_9";
+        if ($news->updateNews($_GET['id'])) {
+            echo "news updated successfuly ^_9";
+        } else {
+            echo "filed to update news !!";
+        }
+        // add
     } else {
-        echo "filed to add news !!";
+        $news->id = $_POST['id'];
+        $news->title = $_POST['title'];
+        $news->image = $_POST['image'];
+        $news->details = $_POST['details'];
+        $news->category_id = $_POST['category_id'];
+
+        if ($news->addNews()) {
+            echo "news added successfuly ^_9";
+        } else {
+            echo "filed to add news !!";
+        }
     }
-    //get  
-} elseif ($method === 'PUT') {
-    if ($news->updateNews($_PUT['id'])) {
-        echo "news updated successfuly ^_9";
+} elseif ($method == "DELETE") {
+    if ($news->deleteNews($_GET['id'])) {
+        echo "news deleted successfuly ^_9";
     } else {
-        echo "filed to update news !!";
-    }
-}
-elseif ($method === 'DELETE') {
-    if ($news->deleteNews($_DELETE['id'])) {
-        echo "news updated successfuly ^_9";
-    } else {
-        echo "filed to update news !!";
+        echo "filed to delete news !!";
     }
 } else {
     if (isset($_GET['id'])) {
